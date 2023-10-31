ce19424efd1182a1f3388e53effe5b84803c585c
Viswanath <viswanath3344@gmail.com>
2023-10-31 22:07:12 +0530

    Initial Commit

---
+++ iOSTest/iOSTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTestApp.swift
+//  iOSTest
+//
+//  Created by Ponthota, Viswanath Reddy (Cognizant) on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
